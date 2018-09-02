ec8e9a95f3215fca004d199c48d47303eedd24c4
Michael Lee Hobbs <michael.lee.hobbs@gmail.com>
2018-09-02 15:20:17 -0400

    added headers to fetch request

--- web/src/modules/authenticate.js
+++ web/src/modules/authenticate.js
@@ -1,5 +1,9 @@
-module.exports = (email, password) => fetch('http://localhost:8080/api/login', {
+module.exports = (email, password) => fetch('/api/login', {
     method: 'POST',
     body: JSON.stringify({email, password}),
     mode: "cors",
+    headers: {
+        "Content-Type": "application/json; charset=utf-8",
+        // "Content-Type": "application/x-www-form-urlencoded",
+    },
 }).then(res => res.json())
